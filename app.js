10a04e691cf7dd7cd678f3cf1ff4eb2e17ee44f2
dhruv1067 <dhruv67d@gmail.com>
2025-04-24 17:42:35 +0530

    winning conditions added

--- app.js
+++ app.js
@@ -8,8 +8,30 @@ const genCompChoice = () => {
     const randomIdx = Math.floor(Math.random() * 3);
     return options[randomIdx];
 }
+
+const drawGame = () => {
+    console.log("game was draw.");
+    
+}
 const playGame = (userChoice) => {
+    console.log("user choice = ", userChoice);
     const compChoice = genCompChoice();
+    console.log("comp choice = ", compChoice);
+
+    if(userChoice === compChoice){
+        drawGame();
+    }else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            userWin = compChoice === "paper" ? false : true;
+        }else if(userChoice ==="paper"){
+           userWin = compChoice === "scissors" ? false : true ;
+        }else{
+            userWin = compChoice === "rock" ? false : true ;
+        }
+
+        showWinner(userWin);
+    }
 }
 
 choices.forEach((element) => {
